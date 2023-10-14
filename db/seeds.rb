05b4818fefc7f2773dc1b1372bf6d2b0d32957d8
NemusD <ch.droguett.l@gmail.com>
2023-10-14 01:49:52 -0300

    Seed/csv tipo de oferta

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
 require 'csv'
-
+#Importar propiedades
 puts 'Importing typeProperties...'
 CSV.foreach(Rails.root.join('db/seeds/csv/typeProperties.csv'), headers: true) do |row|
     TypeProperty.create! do |type_property|
         type_property.id = row[0]
         type_property.name = row[1]
+    end
+end 
+#importar ofertas
+puts 'Importing typeOffers...'
+CSV.foreach(Rails.root.join('db/seeds/csv/typeOffers.csv'), headers: true) do |row|
+    TypeOffer.create! do |type_offer|
+        type_offer.id = row[0]
+        type_offer.name = row[1]
     end
 end
