228f28c31330b3f04f3a68b52ef798521d5698b9
NemusD <ch.droguett.l@gmail.com>
2023-10-14 01:20:37 -0300

    Se agregaron usuarios y tipo de propiedad

---
+++ app/models/type_property.rb
@@ -0,0 +1,5 @@
+class TypeProperty < ApplicationRecord
+    #Validaciones
+    validates :name, presence :true, uniqueness: true
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_14_023650) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_14_041706) do
   create_table "contacts", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "type_properties", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
